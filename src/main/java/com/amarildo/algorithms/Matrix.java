a93caa2e7ce306e4111865da6b895573901b9bd4
Amarildo Aliaj <82412439+amarilz@users.noreply.github.com>
2025-04-18 23:21:32 +0200

    add: Matrix dfs

---
+++ src/main/java/com/amarildo/algorithms/Matrix.java
@@ -0,0 +1,12 @@
+package com.amarildo.algorithms;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Matrix {
+
+    public static int countIslandsDFS(int[][] grid) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/amarildo/algorithms/MatrixTest.java
@@ -0,0 +1,78 @@
+package com.amarildo.algorithms;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatrixTest {
+
+    @Nested
+    @DisplayName("matrix dfs")
+    class MatrixDfs {
+
+        @Test
+        void testSingleIsland() {
+            int[][] grid = {
+                    {1, 1, 0, 0},
+                    {1, 1, 0, 0},
+                    {0, 0, 0, 0},
+                    {0, 0, 0, 0}
+            };
+            assertEquals(1, Matrix.countIslandsDFS(grid));
+        }
+
+        @Test
+        void testMultipleIslands() {
+            int[][] grid = {
+                    {1, 0, 0, 1},
+                    {0, 0, 0, 0},
+                    {1, 0, 1, 1},
+                    {0, 0, 0, 0}
+            };
+            assertEquals(4, Matrix.countIslandsDFS(grid));
+        }
+
+        @Test
+        void testAllWater() {
+            int[][] grid = {
+                    {0, 0, 0},
+                    {0, 0, 0},
+                    {0, 0, 0}
+            };
+            assertEquals(0, Matrix.countIslandsDFS(grid));
+        }
+
+        @Test
+        void testAllLand() {
+            int[][] grid = {
+                    {1, 1},
+                    {1, 1}
+            };
+            assertEquals(1, Matrix.countIslandsDFS(grid));
+        }
+
+        @Test
+        void testEmptyGrid() {
+            int[][] grid = new int[0][0];
+            assertEquals(0, Matrix.countIslandsDFS(grid));
+        }
+
+        @Test
+        void testNullGrid() {
+            int[][] grid = null;
+            assertEquals(0, Matrix.countIslandsDFS(grid));
+        }
+
+        @Test
+        void testDisconnectedSingleCells() {
+            int[][] grid = {
+                    {1, 0, 1},
+                    {0, 1, 0},
+                    {1, 0, 1}
+            };
+            assertEquals(5, Matrix.countIslandsDFS(grid));
+        }
+    }
+}
