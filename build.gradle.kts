3d9451d6ee54370a1280e8dc70fc3795fc2fe218
Amarildo Aliaj <82412439+amarilz@users.noreply.github.com>
2025-06-17 18:45:34 +0200

    add: lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,10 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains:annotations:24.0.0")
-    compileOnly("org.projectlombok:lombok:1.18.36")
-
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+    testCompileOnly("org.projectlombok:lombok:1.18.38")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.1")
     testImplementation("org.junit.platform:junit-platform-launcher:1.12.1")
